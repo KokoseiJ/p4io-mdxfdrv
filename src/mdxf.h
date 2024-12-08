7b19adff26d95a8eee87cb17c7aff5587545122c
Wonjun Jung <kokoseij@gmail.com>
2024-12-07 18:48:33 -0800

    mdxf: fix signature mismatch

--- src/mdxf.h
+++ src/mdxf.h
@@ -9,7 +9,7 @@ bool aciodrv_mdxf_start_auto_get(
 );
 
 bool aciodrv_mdxf_recv_poll(
-	struct aciodrv_device_ctx *device, struct ac_io_mdxf_poll_in *poll_in
+	struct aciodrv_device_ctx *device, uint8_t node_id, struct ac_io_mdxf_poll_in *poll_in
 );
 
 #endif
