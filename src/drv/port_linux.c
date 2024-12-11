1f641a8e0a27fd23f297195d4aca98ba02c8f403
Wonjun Jung <kokoseij@gmail.com>
2024-12-11 10:29:08 -0800

    port_linux: add debug logs

--- src/drv/port_linux.c
+++ src/drv/port_linux.c
@@ -1,4 +1,11 @@
+#define LOG_MODULE "aciodrv-port-linux"
+
 #include "port_linux.h"
+#include "util/log.h"
+#include <errno.h>
+#include <string.h>
+
+#define log_errno(x) log_warning("%s() failed: %d(%s)", x, errno, strerror(errno))
 
 /**
  * Open a serial port for communication with a ACIO device.
@@ -14,26 +21,35 @@ HANDLE aciodrv_port_open(const char *port_path, int baud) {
 	struct termios termios_p;
 	int tioctl;
 
+	log_info("Opening ACIO on %s at %d baud", port_path, baud);
+
 	bauddata = baud_to_speed_t(baud);
 
 	if (bauddata == B0) {
+		log_warning("Invalid baudrate %d", baud);
 		return 0;
 	}
 
 	handle = open(port_path, O_RDWR, O_NOCTTY);
 
 	if (handle < 0) {
+		log_errno("open");
 		return 0;
 	}
 
+	log_info("fd: %d", handle);
+
 	if (tcflush(handle, TCIOFLUSH) < 0) {
+		log_errno("tcflush");
 		goto cleanup;
 	}
 
 	if (tcgetattr(handle, &termios_p) < 0) {
+		log_errno("tcgetattr");
 		goto cleanup;
 	}
 	if (ioctl(handle, TIOCMGET, &tioctl) < 0) {
+		log_errno("ioctl TIOCMGET");
 		goto cleanup;
 	}
 
@@ -42,11 +58,15 @@ HANDLE aciodrv_port_open(const char *port_path, int baud) {
 	handle |= TIOCM_DTR | TIOCM_RTS;
 
 	if (tcsetattr(handle, TCSANOW, (const struct termios *) &termios_p) < 0) {
+		log_errno("tcsetattr");
 		goto cleanup;
 	}
 	if (ioctl(handle, TIOCMSET, &tioctl) < 0) {
+		log_errno("ioctl TIOCMSET");
 		goto cleanup;
 	}
+
+	log_info("Opened ACIO device %s at %d", port_path, handle);
 
 	return handle;
 
@@ -69,10 +89,16 @@ int aciodrv_port_read(HANDLE port_fd, void *bytes, int nbytes) {
 	int read_bytes;
 
 	if (port_fd == 0 || bytes == NULL) {
+		log_warning("bad port/buf");
 		return -1;
 	}
 
 	read_bytes = read(port_fd, bytes, nbytes);
+	if (read_bytes < 0) {
+		log_errno("read");
+		return -1;
+	}
+
 	return read_bytes;
 }
 
@@ -89,10 +115,15 @@ int aciodrv_port_write(HANDLE port_fd, const void *bytes, int nbytes) {
 	int written_bytes;
 
 	if (port_fd == 0 || bytes == NULL) {
+		log_warning("bad port/buf");
 		return -1;
 	}
 
 	written_bytes = write(port_fd, bytes, nbytes);
+	if (written_bytes < 0) {
+		log_errno("write");
+	}
+
 	return written_bytes;
 }
 
