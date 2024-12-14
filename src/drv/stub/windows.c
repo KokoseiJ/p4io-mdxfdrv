00b9f9c8cd9cf1ea0e4710d2da26c78a7aa00303
Wonjun Jung <kokoseij@gmail.com>
2024-12-14 14:59:55 -0800

    windows-stub: disable break because it hangs

--- src/drv/stub/windows.c
+++ src/drv/stub/windows.c
@@ -1,15 +1,21 @@
+#define LOG_MODULE "windows-stub"
+
 #include "windows.h"
+#include <util/log.h>
 #include <sys/ioctl.h>
-#include <asm/termbits.h>
 #include <time.h>
 
 BOOL EscapeCommFunction(HANDLE hFile, DWORD dwFunc) {
-	return ioctl(hFile, dwFunc) == 0;
+	log_info("dwFunc %d stub", dwFunc);
+	// return ioctl(hFile, dwFunc) == 0;
+	return 1;
 }
 
 
 void Sleep(DWORD msec) {
 	struct timespec ts;
+
+	log_info("sleep %d msec", msec);
 
 	ts.tv_sec = msec / 1000;
 	ts.tv_nsec = (msec % 1000) * 1000000;
